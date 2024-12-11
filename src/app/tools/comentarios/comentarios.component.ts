db493dfaca645c7a37d553db3387655439ee937e
Jrodrigo20c <1012520202@unajma.edu.pe>
2024-12-10 23:23:25 -0500

    añadiendo comentarios

---
+++ src/app/tools/comentarios/comentarios.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+
+@Component({
+  selector: 'app-comentarios',
+  standalone: true,
+  imports: [MatIconModule],
+  templateUrl: './comentarios.component.html',
+  styleUrl: './comentarios.component.css'
+})
+export class ComentariosComponent {
+
+}
--- src/app/tools/post/post.component.ts
+++ src/app/tools/post/post.component.ts
@@ -1,7 +1,10 @@
-import { Component, Input, input } from '@angular/core';
+import { Component, Input, input, OnChanges, SimpleChanges } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 import { PostData } from '../../pages/post-feed/post-feed.component';
+import { FirebaseTSFirestore } from 'firebasets/firebasetsFirestore/firebaseTSFirestore';
+import { MatDialog } from '@angular/material/dialog';
+import { ComentariosComponent } from '../comentarios/comentarios.component';
 
 @Component({
   selector: 'app-post',
@@ -10,10 +13,46 @@ import { PostData } from '../../pages/post-feed/post-feed.component';
   templateUrl: './post.component.html',
   styleUrl: './post.component.css'
 })
-export class PostComponent {
+export class PostComponent implements OnChanges {
   @Input() postData?: PostData;
+  nombreCreador: string = 'Usuario desconocido';
+  descripcionCreador: string = 'Sin descripción disponible';
+  firestore = new FirebaseTSFirestore();
 
-  ngOnInit(): void{
+  constructor(private dialog: MatDialog) { }
 
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['postData?'] && this.postData?.creatorId) {
+      this.creadorInfo();
+    }
+  }
+
+  comentarioClick(){
+    this.dialog.open(ComentariosComponent);
+  }
+
+  creadorInfo() {
+    const creatorId = this.postData?.creatorId;
+    if (!creatorId) {
+      console.warn('No se proporcionó un ID de creador.');
+      return;
+    }
+    this.firestore.getDocument(
+      {
+        path: ['usuarios', creatorId],
+        onComplete: result => {
+          const userDocument = result.data() as { [key: string]: any } | undefined;
+          if (userDocument) {
+            this.nombreCreador = userDocument['publicName'] || 'Usuario desconocido';
+            this.descripcionCreador = userDocument['description'] || 'Sin descripción disponible';
+          } else {
+            console.warn(`No se encontró un documento de usuario para el ID: ${creatorId}`);
+          }
+        },
+        onFail: error => {
+          console.error('Error al obtener la información del creador:', error);
+        }
+      }
+    );
   }
 }
